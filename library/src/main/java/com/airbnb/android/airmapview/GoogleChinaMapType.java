773a054a36665f9250da6cc339c2b129f6785743
Felipe Lima <felipe.lima@gmail.com>
2015-03-05 16:34:21 -0800

    More public constructors

--- library/src/main/java/com/airbnb/android/airmapview/GoogleChinaMapType.java
+++ library/src/main/java/com/airbnb/android/airmapview/GoogleChinaMapType.java
@@ -2,7 +2,7 @@ package com.airbnb.android.airmapview;
 
 public class GoogleChinaMapType extends AirMapType {
 
-    GoogleChinaMapType() {
+    public GoogleChinaMapType() {
         super("google_map.html", "http://ditu.google.cn/maps/api/js", "www.google.cn");
     }
 }
--- library/src/main/java/com/airbnb/android/airmapview/GoogleWebMapType.java
+++ library/src/main/java/com/airbnb/android/airmapview/GoogleWebMapType.java
@@ -1,7 +1,7 @@
 package com.airbnb.android.airmapview;
 
 public class GoogleWebMapType extends AirMapType {
-    GoogleWebMapType() {
+    public GoogleWebMapType() {
         super("google_map.html", "https://maps.googleapis.com/maps/api/js", "www.googleapis.com");
     }
 }
