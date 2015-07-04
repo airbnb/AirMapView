b6cedc54aefc25e7c068fff01b6950fe05498743
Brad Leege <bleege@gmail.com>
2015-07-04 16:48:40 -0500

    Updating to Mapbox JS 2.2.1

--- library/src/main/java/com/airbnb/android/airmapview/MapboxWebMapType.java
+++ library/src/main/java/com/airbnb/android/airmapview/MapboxWebMapType.java
@@ -3,6 +3,6 @@ package com.airbnb.android.airmapview;
 public class MapboxWebMapType extends AirMapType
 {
     public MapboxWebMapType() {
-        super("mapbox.html", "https://api.tiles.mapbox.com/mapbox.js/v2.1.9", "www.mapbox.com");
+        super("mapbox.html", "https://api.tiles.mapbox.com/mapbox.js/v2.2.1", "www.mapbox.com");
     }
 }
