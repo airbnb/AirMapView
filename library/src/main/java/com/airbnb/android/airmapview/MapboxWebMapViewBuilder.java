2d2f20bb9b7d805a6d82920d0d44da1b767d5ed5
Brad Leege <bleege@gmail.com>
2015-07-07 17:18:42 -0500

    Code style changes

--- library/src/main/java/com/airbnb/android/airmapview/MapboxWebMapViewBuilder.java
+++ library/src/main/java/com/airbnb/android/airmapview/MapboxWebMapViewBuilder.java
@@ -1,42 +1,45 @@
 package com.airbnb.android.airmapview;
 
+/**
+ * AirMapView map that uses the web based Mapbox implementation.
+ */
 public class MapboxWebMapViewBuilder implements AirMapViewBuilder<WebViewMapFragment, AirMapType> {
 
-    private AirMapType options;
-    private final String accessToken;
-    private final String mapId;
+  private AirMapType options;
+  private final String accessToken;
+  private final String mapId;
 
-    /**
-     * Constructor
-     *
-     * @param accessToken Mapbox Access Token
-     * @param mapId Mapbox Map Id
-     */
-    public MapboxWebMapViewBuilder(String accessToken, String mapId) {
-      super();
-      this.accessToken = accessToken;
-      this.mapId = mapId;
-    }
+  /**
+   * Constructor
+   *
+   * @param accessToken Mapbox Access Token
+   * @param mapId Mapbox Map Id
+   */
+  public MapboxWebMapViewBuilder(String accessToken, String mapId) {
+    super();
+    this.accessToken = accessToken;
+    this.mapId = mapId;
+  }
 
-    @Override
-    public AirMapViewBuilder<WebViewMapFragment, AirMapType> withOptions(AirMapType options) {
-        this.options = options;
-        return this;
-    }
+  @Override
+  public AirMapViewBuilder<WebViewMapFragment, AirMapType> withOptions(AirMapType options) {
+    this.options = options;
+    return this;
+  }
 
-    /**
-     * Build the map fragment with the requested options
-     *
-     * @return The {@link WebViewMapFragment} map fragment.
-     */
-    @Override
-    public WebViewMapFragment build() {
-        if (options == null) {
-            options = new MapboxWebMapType(accessToken, mapId);
-        }
-        if (options instanceof MapboxWebMapType) {
-            return MapboxWebViewMapFragment.newInstance(options);
-        }
-        return null;
+  /**
+   * Build the map fragment with the requested options
+   *
+   * @return The {@link WebViewMapFragment} map fragment.
+   */
+  @Override
+  public WebViewMapFragment build() {
+    if (options == null) {
+      options = new MapboxWebMapType(accessToken, mapId);
     }
+    if (options instanceof MapboxWebMapType) {
+      return MapboxWebViewMapFragment.newInstance(options);
+    }
+    return null;
+  }
 }
